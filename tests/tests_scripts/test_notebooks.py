3d9bbb1bf03da815864a3007d334d44a8d77f752
Luís F. Pereira <luisfgper@gmail.com>
2024-08-09 19:24:35 +0200

    Add tests ensuring notebooks run

---
+++ tests/tests_scripts/test_notebooks.py
@@ -0,0 +1,16 @@
+"""Unit tests for the notebooks."""
+
+import glob
+
+from geomstats.test.parametrizers import NotebooksParametrizer
+from geomstats.test.test_case import TestCase
+
+
+class NotebooksTestData:
+    def __init__(self):
+        NOTEBOOKS_DIR = "notebooks"
+        self.paths = sorted(glob.glob(f"{NOTEBOOKS_DIR}/**/*.ipynb", recursive=True))
+
+
+class TestNotebooks(TestCase, metaclass=NotebooksParametrizer):
+    testing_data = NotebooksTestData()
