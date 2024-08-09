9407bcea14051486621554871db72024fd466617
Luís F. Pereira <luisfgper@gmail.com>
2024-08-09 13:27:19 +0200

    Add io utils

---
+++ fmib/io.py
@@ -0,0 +1,18 @@
+import meshio
+
+
+def load_mesh(filename):
+    """Load a mesh from a file.
+
+    Parameters
+    ----------
+    filename : str
+        File name.
+
+    Returns
+    -------
+    vertices : array-like, shape=[n_vertices, 3]
+    faces : array_like, shape=[n_faces, 3]
+    """
+    mesh = meshio.read(filename)
+    return mesh.points, mesh.cells[0].data
