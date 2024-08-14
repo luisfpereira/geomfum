0ec79ec6d20055a107043d719b68cf57564cbde0
Luís F. Pereira <luisfgper@gmail.com>
2024-08-14 14:54:30 +0200

    Add bijective converters from Giulio's code

--- geomfum/convert.py
+++ geomfum/convert.py
@@ -1,19 +1,35 @@
 """Conversion between pointwise and functional maps."""
 
+import abc
+
 import scipy
 from sklearn.neighbors import NearestNeighbors
 
 
-class P2pFromFmConverter:
+class BaseP2pFromFmConverter(abc.ABC):
+    """Pointwise map from functional map."""
+
+
+class P2pFromFmConverter(BaseP2pFromFmConverter):
     """Pointwise map from functional map.
 
     Parameters
     ----------
-    n_neighbors : int
-         Ignored if ``neighbor_finder`` is not None.
+    neighbor_finder : NeighborFinder
+        Nearest neighbor finder.
+    adjoint : bool
+        Whether to use adjoint method.
+    bijective : bool
+        Whether to use bijective method. Check [VM2023]_.
+
+    References
+    ----------
+    .. [VM2023] Giulio Viganò  Simone Melzi. “Adjoint Bijective ZoomOut:
+        Efficient Upsampling for Learned Linearly-Invariant Embedding.”
+        The Eurographics Association, 2023. https://doi.org/10.2312/stag.20231293.
     """
 
-    def __init__(self, neighbor_finder=None, use_adjoint=False):
+    def __init__(self, neighbor_finder=None, adjoint=False, bijective=False):
         if neighbor_finder is None:
             neighbor_finder = NearestNeighbors(
                 n_neighbors=1, leaf_size=40, algorithm="kd_tree", n_jobs=1
@@ -22,7 +38,8 @@ class P2pFromFmConverter:
             raise ValueError("Expects `n_neighors = 1`.")
 
         self.neighbor_finder = neighbor_finder
-        self.use_adjoint = use_adjoint
+        self.adjoint = adjoint
+        self.bijective = bijective
 
     def __call__(self, fmap_matrix, basis_a, basis_b):
         """Convert functional map.
@@ -34,11 +51,12 @@ class P2pFromFmConverter:
 
         Returns
         -------
-        p2p : array-like, shape=[n_vertices_b]
+        p2p : array-like, shape=[{n_vertices_b, n_vertices_a}]
+            Pointwise map. ``bijective`` controls shape.
         """
         k2, k1 = fmap_matrix.shape
 
-        if self.use_adjoint:
+        if self.adjoint:
             emb1 = basis_a.full_vecs[:, :k1]
             emb2 = basis_b.full_vecs[:, :k2] @ fmap_matrix
 
@@ -46,19 +64,31 @@ class P2pFromFmConverter:
             emb1 = basis_a.full_vecs[:, :k1] @ fmap_matrix.T
             emb2 = basis_b.full_vecs[:, :k2]
 
+        if self.bijective:
+            emb1, emb2 = emb2, emb1
+
         self.neighbor_finder.fit(emb1)
         _, p2p_21 = self.neighbor_finder.kneighbors(emb2)
 
-        # TODO: check shape
         return p2p_21[:, 0]
 
 
-class FmFromP2pConverter:
+class BaseFmFromP2pConverter(abc.ABC):
     """Functional map from pointwise map."""
 
+
+class FmFromP2pConverter(BaseFmFromP2pConverter):
+    """Functional map from pointwise map.
+
+    Parameters
+    ----------
+    pseudo_inverse : bool
+        Whether to solve using pseudo-inverse.
+    """
+
     # TODO: add subsampling
-    def __init__(self, use_area=False):
-        self.use_area = use_area
+    def __init__(self, pseudo_inverse=False):
+        self.pseudo_inverse = pseudo_inverse
 
     def __call__(self, p2p, basis_a, basis_b):
         """Convert point to point map.
@@ -66,6 +96,7 @@ class FmFromP2pConverter:
         Parameters
         ----------
         p2p : array-like, shape=[n_vertices_b]
+            Poinwise map.
 
         Returns
         -------
@@ -74,8 +105,35 @@ class FmFromP2pConverter:
         """
         evects1_pb = basis_a.vecs[p2p, :]
 
-        if self.use_area:
+        if self.pseudo_inverse:
             # TODO: give access to mass_matrix to basis?
             return basis_b.vecs.T @ (basis_b._shape.mass_matrix @ evects1_pb)
 
         return scipy.linalg.lstsq(basis_b.vecs, evects1_pb)[0]
+
+
+class FmFromP2pBijectiveConverter(BaseFmFromP2pConverter):
+    """Bijective functional map from pointwise map method.
+
+    References
+    ----------
+    .. [VM2023] Giulio Viganò  Simone Melzi. “Adjoint Bijective ZoomOut:
+        Efficient Upsampling for Learned Linearly-Invariant Embedding.”
+        The Eurographics Association, 2023. https://doi.org/10.2312/stag.20231293.
+    """
+
+    def __call__(self, p2p, basis_a, basis_b):
+        """Convert point to point map.
+
+        Parameters
+        ----------
+        p2p : array-like, shape=[n_vertices_a]
+            Pointwise map.
+
+        Returns
+        -------
+        fmap_matrix : array-like, shape=[spectrum_size_b, spectrum_size_a]
+            Functional map matrix.
+        """
+        evects2_pb = basis_b.vecs[p2p, :]
+        return scipy.linalg.lstsq(evects2_pb, basis_a.vecs)[0]
