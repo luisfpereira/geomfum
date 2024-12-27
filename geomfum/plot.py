ea68edd1c8bdedeb5af45133da9d568593e84f7e
gviga <giulio.vigan@gmail.com>
2024-12-27 10:13:04 +0100

    docstrings

--- geomfum/plot.py
+++ geomfum/plot.py
@@ -1,26 +1,28 @@
 import abc
 from geomfum._registry import MeshPlotterRegistry, WhichRegistryMixins
 
-'''
-In this file i define the plotting logic used in geomfum
+"""
+In this file we define the plotting logic used in geomfum
 Since each plotting library has its own method of plotting, we define general functions that works with any library implemented 
 
-'''
+"""
 
 class ShapePlotter(abc.ABC):
-    '''
+    """
     Primitive Calss to plot meshes, pointclouds or specific useful informations (scalar functions, landmarks, etc..)
-    '''
+    """
     def plot(self, mesh):
-        '''
+        
+        """
         Function to plot a mesh
-        '''
+        
+        """
         raise NotImplementedError("This method should be overridden by subclasses")
     
-    def plot_function(self, mesh, function):
-        '''
+    def plot_function(self, mesh, function):  
+        """
         Function to plot a mesh
-        '''
+        """
         raise NotImplementedError("This method should be overridden by subclasses")
 
 class MeshPlotter(WhichRegistryMixins, ShapePlotter):
