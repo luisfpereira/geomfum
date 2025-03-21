69d9f183f03588e282f3575e5f8697e2862fb8d9
gviga <giulio.vigan@gmail.com>
2025-03-21 09:33:31 +0100

    Update polyscope

--- geomfum/wrap/polyscope.py
+++ geomfum/wrap/polyscope.py
@@ -7,32 +7,33 @@ class PolyscopeMeshPlotter(ShapePlotter):
         self.colormap = colormap
         self.fig = None
         self.name=name
-        ps.init()
 
     def plot(self, mesh):
         """
         Plot the mesh using Polyscope.
         """
-        ps_mesh = ps.register_surface_mesh(self.name,mesh.vertices,mesh.faces)
-        self.fig = ps_mesh
+        ps.init()
+        ps.register_surface_mesh(self.name,mesh.vertices,mesh.faces)
+        self.fig = ps
         return self.fig
 
     def plot_function(self, mesh, function):
         """
         Plot the mesh with a scalar function using Polyscope.
         """
-        ps_mesh = ps.register_surface_mesh(
+        ps.init()
+        ps.register_surface_mesh(
             self.name,
             mesh.vertices,
             mesh.faces,
         )
-        ps_mesh.add_scalar_quantity("my_scalar", function, defined_on='vertices', cmap=self.colormap)
+        ps.add_scalar_quantity("my_scalar", function, defined_on='vertices', cmap=self.colormap)
 
-        self.fig = ps_mesh
+        self.fig = ps
         return self.fig
 
     def show(self):
         """
         Show the Polyscope visualization.
         """
-        ps.show()
+        self.fig.show()
