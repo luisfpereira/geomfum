df13affd03daaacdc92e4457e9ede00fdd1dc210
gviga <giulio.vigan@gmail.com>
2025-03-26 14:07:20 +0100

    Implementing Pointcloud attributes

--- geomfum/shape/point_cloud.py
+++ geomfum/shape/point_cloud.py
@@ -1,5 +1,6 @@
 """Definition of point cloud."""
 
+from geomfum.io import load_mesh
 from ._base import Shape
 
 
@@ -15,3 +16,25 @@ class PointCloud(Shape):
     def __init__(self, vertices):
         super().__init__(is_mesh=False)
         self.vertices = vertices
+
+    @classmethod
+    def from_file(cls, filename):
+        """Instantiate given a file.
+
+        Returns
+        -------
+        mesh : TriangleMesh
+            A triangle mesh.
+        """
+        vertices, _ = load_mesh(filename)
+        return cls(vertices)
+
+    @property
+    def n_vertices(self):
+        """Number of vertices.
+
+        Returns
+        -------
+        n_vertices : int
+        """
+        return self.vertices.shape[0]
