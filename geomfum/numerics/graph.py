8736d9c70d27b86f6cbe52f2742bb49a552b00ec
Luís F. Pereira <luisfgper@gmail.com>
2025-04-29 12:49:44 -0700

    Fix single source partial dijkstra

--- geomfum/numerics/graph.py
+++ geomfum/numerics/graph.py
@@ -41,7 +41,7 @@ def single_source_partial_dijkstra_path_length(graph, source, k, weight="weight"
 
         for v, edata in graph[u].items():
             if v not in visited:
-                w = edata.get(weight)
+                w = weight(u, v, edata)
                 heapq.heappush(heap, (dist_u + w, v))
 
     return length
