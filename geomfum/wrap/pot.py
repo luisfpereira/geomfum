10486da621855080bc9ef9d5701a9d67c251853c
gviga <giulio.vigan@gmail.com>
2025-05-12 14:22:46 +0200

    update sinkhorn

--- geomfum/wrap/pot.py
+++ geomfum/wrap/pot.py
@@ -25,9 +25,10 @@ class PotSinkhornNeighborFinder(BaseSinkhornNeighborFinder):
         Maximum number of iterations for Sinkhorn algorithm.
     """
 
-    def __init__(self, n_neighbors=1, lambd=1e-1):
+    def __init__(self, n_neighbors=1, lambd=1e-2, max_iter=100):
         self.n_neighbors = n_neighbors
         self.lambd = lambd
+        self.max_iter = max_iter
         self.X = None
 
     def fit(self, X):
@@ -57,13 +58,13 @@ class PotSinkhornNeighborFinder(BaseSinkhornNeighborFinder):
             Indices of the nearest neighbors.
         """   
       
-        M = ot.dist(Y, self.X)
+        M = np.exp(-self.lambd*ot.dist(Y, self.X))
         n, m = M.shape
         
         a = np.ones(n) / n
         b = np.ones(m) / m
 
-        Gs = ot.sinkhorn(a, b, M, self.lambd)   
+        Gs = ot.sinkhorn(a, b, M, self.lambd,numItermax=self.max_iter)   
              
         indices = np.argsort(Gs, axis=1)[:, :self.n_neighbors]
         
