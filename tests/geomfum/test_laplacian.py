51c54476e6a21418a4a2cd5f991b3851ad2174df
gviga <giulio.vigan@gmail.com>
2025-05-15 15:43:38 +0200

    test_fixing

--- tests/geomfum/test_laplacian.py
+++ tests/geomfum/test_laplacian.py
@@ -24,9 +24,8 @@ class TestLaplacianFinderCmp(LaplacianFinderCmpCase, metaclass=DataBasedParametr
     Redundant with ``TestLaplacianSpectrumFinderCmp``.
     """
 
-    finder_a = LaplacianFinder()
-    finder_b = LaplacianFinder.from_registry(which="pyfm")
-    finder_c = LaplacianFinder.from_registry(which="igl")
+    finder_a = LaplacianFinder.from_registry(which="pyfm")
+    finder_b = LaplacianFinder.from_registry(which="igl")
 
     testing_data = LaplacianFinderCmpData()
 
@@ -46,7 +45,7 @@ def spectrum_finders(request):
     which_a, which_b = request.param
 
     spectrum_size = random.randint(2, 5)
-    if which_a=='default':
+    if which_a == "default":
         request.cls.finder_a = LaplacianSpectrumFinder(
             spectrum_size=spectrum_size,
             laplacian_finder=LaplacianFinder(),
@@ -61,7 +60,7 @@ def spectrum_finders(request):
         request.cls.finder_b = LaplacianSpectrumFinder(
             spectrum_size=spectrum_size,
             laplacian_finder=LaplacianFinder(),
-        )   
+        )
     else:
         request.cls.finder_b = LaplacianSpectrumFinder(
             spectrum_size=spectrum_size,
