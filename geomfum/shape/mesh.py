82b80655edf8e53d40e1b8fff4f3caf5a9bf3c99
Luís F. Pereira <luisfgper@gmail.com>
2025-05-22 12:57:13 -0700

    Update mesh main methods to work with gs/gf

--- geomfum/shape/mesh.py
+++ geomfum/shape/mesh.py
@@ -1,8 +1,8 @@
 """Definition of triangle mesh."""
 
-import numpy as np
-import scipy
+import geomstats.backend as gs
 
+import geomfum.backend as gf
 from geomfum.io import load_mesh
 from geomfum.operator import (
     FaceDivergenceOperator,
@@ -26,8 +26,8 @@ class TriangleMesh(Shape):
 
     def __init__(self, vertices, faces):
         super().__init__(is_mesh=True)
-        self.vertices = np.asarray(vertices)
-        self.faces = np.asarray(faces)
+        self.vertices = gs.asarray(vertices)
+        self.faces = gs.asarray(faces)
 
         self._edges = None
         self._face_normals = None
@@ -88,30 +88,41 @@ class TriangleMesh(Shape):
         edges : array-like, shape=[n_edges, 2]
         """
         if self._edges is None:
-            vind012 = np.concatenate(
+            vind012 = gs.concatenate(
                 [self.faces[:, 0], self.faces[:, 1], self.faces[:, 2]]
             )
-            vind120 = np.concatenate(
+            vind120 = gs.concatenate(
                 [self.faces[:, 1], self.faces[:, 2], self.faces[:, 0]]
             )
-
-            E1 = np.concatenate([vind012, vind120])
-            E2 = np.concatenate([vind120, vind012])
-            W = np.ones_like(E1)
-
-            M = scipy.sparse.csr_matrix(
-                (W, (E1, E2)), shape=(self.n_vertices, self.n_vertices)
-            ).tocoo()
-
-            edges0 = M.row
-            edges1 = M.col
-
-            indices = M.col > M.row
-
-            self._edges = np.concatenate(
-                [edges0[indices, None], edges1[indices, None]], axis=1
+            edges = gs.stack(
+                [
+                    gs.concatenate([vind012, vind120]),
+                    gs.concatenate([vind120, vind012]),
+                ],
+                axis=-1,
             )
+            edges = gs.unique(edges, axis=0)
+            self._edges = edges[edges[:, 1] > edges[:, 0]]
+
         return self._edges
+
+    @property
+    def face_vertex_coords(self):
+        """Extract vertex coordinates corresponding to each face.
+
+        Returns
+        -------
+        vertices : array-like, shape=[{n_faces}, n_per_face_vertex, 3]
+            Coordinates of the ith vertex of that face.
+        """
+        return gs.stack(
+            [
+                self.vertices[self.faces[:, 0]],
+                self.vertices[self.faces[:, 1]],
+                self.vertices[self.faces[:, 2]],
+            ],
+            axis=-2,
+        )
 
     @property
     def face_normals(self):
@@ -120,19 +131,29 @@ class TriangleMesh(Shape):
         Returns
         -------
         normals : array-like, shape=[n_faces, 3]
-            Normalized per-face normals.
+            Per-face normals.
         """
         if self._face_normals is None:
-            v1 = self.vertices[self.faces[:, 0]]
-            v2 = self.vertices[self.faces[:, 1]]
-            v3 = self.vertices[self.faces[:, 2]]
-
-            normals = np.cross(v2 - v1, v3 - v1)
-            normals /= np.linalg.norm(normals, axis=1, keepdims=True)
-
-            self._face_normals = normals
+            face_vertex_coords = self.face_vertex_coords
+            self._face_normals = gs.cross(
+                face_vertex_coords[:, 1, :] - face_vertex_coords[:, 0, :],
+                face_vertex_coords[:, 2, :] - face_vertex_coords[:, 0, :],
+            )
 
         return self._face_normals
+
+    @property
+    def unit_face_normals(self):
+        """Compute face normals of a triangular mesh.
+
+        Returns
+        -------
+        normals : array-like, shape=[n_faces, 3]
+            Per-face normals.
+        """
+        return self.face_normals / gs.linalg.norm(
+            self.face_normals, axis=1, keepdims=True
+        )
 
     @property
     def face_areas(self):
@@ -144,10 +165,7 @@ class TriangleMesh(Shape):
             Per-face areas.
         """
         if self._face_areas is None:
-            v1 = self.vertices[self.faces[:, 0]]
-            v2 = self.vertices[self.faces[:, 1]]
-            v3 = self.vertices[self.faces[:, 2]]
-            self._face_areas = 0.5 * np.linalg.norm(np.cross(v2 - v1, v3 - v1), axis=1)
+            self._face_areas = 0.5 * gs.linalg.norm(self.face_normals, axis=1)
 
         return self._face_areas
 
@@ -162,19 +180,15 @@ class TriangleMesh(Shape):
         vertex_areas : array-like, shape=[n_vertices]
             Per-vertex areas.
         """
-        if self._vertex_areas is None:
-            # THIS IS JUST A TRICK TO BE FASTER THAN NP.ADD.AT
-            vind012 = np.concatenate(
-                [self.faces[:, 0], self.faces[:, 1], self.faces[:, 2]]
-            )
-            vind120 = np.zeros_like(vind012)
+        area = self.face_areas
 
-            areas = np.tile(self.face_areas / 3, 3)
-
-            self._vertex_areas = np.array(
-                scipy.sparse.coo_matrix(
-                    (areas, (vind012, vind120)), shape=(self.n_vertices, 1)
-                ).todense()
-            ).flatten()
-
-        return self._vertex_areas
+        id_vertices = gs.broadcast_to(gs.reshape(self.faces, (-1,)), self.n_faces * 3)
+        val = gs.reshape(
+            gs.broadcast_to(gs.expand_dims(area, axis=-1), (self.n_faces, 3)),
+            (-1,),
+        )
+        incident_areas = gf.scatter_sum_1d(
+            index=id_vertices,
+            src=val,
+        )
+        return incident_areas / 3.0
