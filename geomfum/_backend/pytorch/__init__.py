0ca1a1976c02b0902823588610cb79188a4e2961
Luís F. Pereira <luisfgper@gmail.com>
2025-05-22 12:54:38 -0700

    Add geomstats-like backend for fast development

---
+++ geomfum/_backend/pytorch/__init__.py
@@ -0,0 +1,9 @@
+import torch as _torch
+
+
+def scatter_sum_1d(index, src, size=None):
+    if size is None:
+        size = index.max() + 1
+
+    array = _torch.zeros(size, dtype=src.dtype)
+    return _torch.scatter_add(array, -1, index, src)
