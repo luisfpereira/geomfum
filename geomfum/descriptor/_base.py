3acb2cc7a3bbc98d407e46a67c776add93d43ea2
gviga <giulio.vigan@gmail.com>
2025-07-01 15:47:24 +0000

    Implementation of distance from landmarks descriptors

--- geomfum/descriptor/_base.py
+++ geomfum/descriptor/_base.py
@@ -1,4 +1,5 @@
 import abc
+import geomstats.backend as gs
 
 
 class Descriptor(abc.ABC):
@@ -33,3 +34,31 @@ class SpectralDescriptor(Descriptor, abc.ABC):
         domain : array-like, shape=[n_domain]
             Domain points for computation.
         """
+
+
+class DistanceFromLandmarksDescriptor(Descriptor):
+    """Distance from landmarks descriptor. A simple descriptor that returns the distance from landmarks as a function on the shape."""
+
+    def __call__(self, shape):
+        """Compute descriptor.
+
+        Parameters
+        ----------
+        shape : Shape.
+            Shape.
+
+        Returns
+        -------
+        descriptor : array-like, shape=[n_landmarks]
+            Descriptor values.
+        """
+        if not hasattr(shape, "landmark_indices"):
+            raise AttributeError(
+                "shape object does not have 'landmark_indices' attribute"
+            )
+
+        if shape.metric is None:
+            raise ValueError("shape is not equipped with metric")
+        distances_list = shape.metric.dist_from_source(shape.landmark_indices)[0]
+        distances = gs.stack(distances_list)
+        return distances
